44f96c36bbcd8b3b3bf19692f8ed53572c4d62e7
LxaNce <96795383+LxaNce-Hacker@users.noreply.github.com>
2025-01-27 17:56:07 +0530

    Easy
    Given an integer x, return true if x is a
    palindrome
    , and false otherwise.
    
     
    
    Example 1:
    
    Input: x = 121
    Output: true
    Explanation: 121 reads as 121 from left to right and from right to left.
    
    Example 2:
    
    Input: x = -121
    Output: false
    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
    
    Example 3:
    
    Input: x = 10
    Output: false
    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
    
     
    
    Constraints:
    
        -231 <= x <= 231 - 1
    
     
    Follow up: Could you solve it without converting the integer to a string?

---
+++ LeetCode/Problems/1. Two Sum/9. Palindrome Number/palindrome-number.c
@@ -0,0 +1,13 @@
+bool isPalindrome(int x) {
+    if(x<0) return false;
+
+    int rev=0,temp, org=x;
+    while(x!=0)
+    {
+        temp=x%10;
+        rev=rev*10+temp;
+        x=x/10;
+    }
+
+    return rev==org;
+}
